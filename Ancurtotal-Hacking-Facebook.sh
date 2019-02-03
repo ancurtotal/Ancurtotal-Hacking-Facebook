cc0b856be8d5b989f96012430619f4ec47f01c85
Ekay Ancurtotal <41782306+ancurtotal@users.noreply.github.com>
2019-02-03 20:39:21 +0700

    Add files via upload

---
+++ Ancurtotal-Hacking-Facebook.sh
@@ -0,0 +1,295 @@
+#!/bin/bash
+#version 6.0
+
+# MAU NGAPAIN JINK??
+# DI RECODE GAK BAKAL JADIIN GUE NEWBIE DAN LO MASTAH!!!
+# DI GANTI AUTHOR GAK BAKAL JADIIN GUE NOOB DAN LO PRO!!!
+# MIKIR PAKE DENGKUL BRO JANGAN PAKE OTAK!!!
+# JANGAN REEDIT ATAU RECODE KECUALI KALO LO GAK ADA OTAKNYA!!!
+# KALO LOE GAK PAHAM SILAHKAN BELAJAR BARENG SAMA GUE DI ANCURTOTAL CYBER TEAM!!!
+
+#03/02/19
+
+clear
+python2 ancurtotal.py
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+lolcat Ekay.txt
+figlet ANCURTOTAL | lolcat
+
+echo  "_____________________________________________________________" | lolcat
+echo  "TYPE      : TOOLS INSTALLER " |lolcat
+echo  "VERSION   : V.6 " | lolcat
+echo  "TOTALS    : 6 TOOLS " | lolcat
+echo  "AUTHOR    : EKAY ANCURTOTAL " |lolcat
+echo  "ASSOCIATE : ANCURTOTAL CYBER TEAM " |lolcat
+echo  "ASSOCIATE : SCARY OF CYBER DEMON " |lolcat
+echo  "ASSOCIATE : BUFT TEAM " |lolcat
+echo  "ASSOCIATE : MUSLIM CYBER ARMY " |lolcat
+echo  "NOTES     : JELAJAHI SETIAP TOOLS DENGAN BIJAK " | lolcat
+echo  "THANKS TO : ALLAH SWT AND RASULULLAH SAW" | lolcat
+echo  "THANKS TO : MY WIFE AND TWO DAUGHTERS" | lolcat
+echo  "_____________________________________________________________" | lolcat
+
+sleep 1
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUE " |lolcat
+echo  "Thanks For Using My Tools" |lolcat
+sleep 1
+echo  "EKAY ANCURTOTAL On Your System" |lolcat
+
+echo  "See you Again SCRIPTKIDDIES😎 :)..." |lolcat
+echo
+echo  "AIM FOR SOMETHING THAT IS IMPOSSIBLE"
+echo  "THERE IS NO SAFE SYSTEM"
+echo  "HAVE FUN IN CYBERSPACE AND THE REAL WORLD"
+echo  "HACKING IS NOT ALWAYS A CRIME :v"
+figlet ANCURTOTAL | lolcat
+echo "                 #  ANCURTOTAL CYBER TEAM # " | lolcat
+sleep 1
+exit
+}
+
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+
+echo ""                
+echo ""                
+
+echo  "######################################" | lolcat
+echo  "#SILAHKAN TUNGGU DAN LIHAT LIST TOOLS#" | lolcat
+echo  "######################################" | lolcat
+
+echo ""                
+echo  "TOOLS INI CUMA UNTUK MENGINSTALLNYA SAJA" |lolcat
+echo  "JIKA MAU MENJALANKAN TOOLS YANG KALIAN INSTALL" | lolcat
+echo  "SILAHKAN KETIK 00 LALU KETIK ls" | lolcat
+echo  "DAN MASUK KE TOOLS YANG KALIAN INSTALL" | lolcat
+echo  "JANGAN NANYA LAGI JINK " | lolcat
+echo  "SABAR YA NUNGGU NYA :v " | lolcat
+echo
+echo  "NGOPI!!! " | lolcat
+echo
+echo  "#TTD EKAY ANCURTOTAL 😎" | lolcat
+echo  "#ORANG GILA MAH BEBAS😎" | lolcat
+echo ""  
+echo ""  
+
+echo "######################################" | lolcat
+echo "####  ANCURTOTAL HACKING FACEBOOK  ###" | lolcat
+echo "######################################" | lolcat
+
+echo ""
+echo "============================" | lolcat
+echo "1. Install Multi Brute Force Versi Satu (MBFv1)"
+echo "============================" | lolcat
+echo "2. Install Multi Brute Force Versi Dua (MBFv2)"
+echo "============================" | lolcat
+echo "3. Install FbBrute Versi Satu (FbBruteV1)"
+echo "============================" | lolcat
+echo "4. Install FbBrute Versi Dua (FbBruteV2)"
+echo "============================" | lolcat
+echo "5. Install FbBrute Versi Tiga (FbBruteV3)"
+echo "============================" | lolcat
+echo "6. Install Cupp Tool Untuk Bikin WorldList"
+echo "============================" | lolcat
+echo "00. Exit"
+echo "============================" | lolcat
+echo "     [PILIH NOMORNYA BRO]"
+echo
+read -p "Ancurtotal@Hacking-Facebook ~>>" pil;
+
+#Install Multi Brute Force Versi Satu (MBFv1)
+
+case $pil in
+1) clear
+echo "Install Multi Brute Force Versi Satu (MBFv1)...Installing, Please Wait !!" | lolcat
+sleep 1
+apt update
+apt upgrade 
+apt install python2
+apt install python2-dev
+pip2 install mechanize
+pkg install git
+git clone https://github.com/ancurtotal/MBFv1
+cd MBFv1
+chmod +x MBF.py
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Multi Brute Force Versi Satu (MBFv1) :"
+echo " $ cd MBFv1"
+echo " $ chmod +x MBF.py"
+echo " $ python2 MBF.py"
+
+
+;;
+
+#Install Multi Brute Force Versi Dua (MBFv2)
+
+2) clear
+echo "Install Multi Brute Force Versi Dua (MBFv2)...Installing, Please Wait !!" | lolcat
+sleep 1
+apt update
+apt upgrade
+apt install python2
+apt install python2-dev
+pip2 install mechanize
+pkg install git
+git clone https://github.com/ancurtotal/MBFv2
+cd MBFv2
+chmod +x MBF2.py
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Multi Brute Force Versi Dua (MBFv2) :"
+echo " $ cd MBFv2"
+echo " $ chmod +x MBF2.py"
+echo " $ python2 MBF2.py"
+
+
+;;
+
+#Install FbBrute Versi Satu (FbBruteV1)
+
+3) clear
+echo "Install FbBrutr Versi Satu (FbBruteV1)...Installing, Please Wait !!" | 
+lolcat
+sleep 1
+apt update
+apt upgrade
+apt install python2
+apt install python2-dev
+pip2 install mechanize
+pkg install git
+git clone https://github.com/ancurtotal/FbBruteV1
+cd FbBruteV1
+chmod +x fbbrute.py
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan FbBrute Versi Satu (FbBruteV1) :"
+echo " $ cd FbBruteV1"
+echo " $ chmod +x fbbrute.py"
+echo " $ python2 fbbrute.py"
+
+
+;;
+
+#Install FbBrute Versi Dua (FbBruteV2)
+
+4) clear
+echo "Install FbBrute Versi Dua (FbBruteV2)...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/ancurtotal/FbBruteV2
+cd FbBruteV2
+chmod +x fbbrute2.py
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan FbBrute Versi Satu (FbBruteV1) :"
+echo " $ cd FbBruteV2"
+echo " $ chmod +x fbbrute2.py"
+echo " $ python2 fbbrute2.py"
+
+;;
+
+#Install FbBrute Versi Tiga (FbBruteV3)
+
+5) clear
+echo "Install FbBrute Versi Tiga (FbBruteV3)...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/ancurtotal/FbBruteV3
+cd FbBruteV3
+chmod +x fbbrute3.py
+echo
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Routerspoit :"
+echo " $ cd FbBruteV3"
+echo " $ chmod +x fbbrute3.py"
+echo " $ python2 fbbrute3.py"
+
+
+;;
+
+#Install Cupp Tool Untuk Bikin WorldLits
+
+6) clear
+echo "Instsll Cupp Tool Untuk Bikin WorldlLits...Installing, Please Wait !!" | lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Mebus/cupp.git
+cd cupp
+chmod +x cupp.py
+echo "SUDAH TERINSTALL BRO!!" | lolcat
+echo
+echo "Cara Menggunakan Cupp Tool Untuk Bikin WorldList :"
+echo " $ cd cupp"
+echo " $ chmod +x cupp.py"
+echo " $ python2 cupp.py"
+
+
+;;
+
+00) lolcat ThanksYou.txt
+echo
+
+echo  "TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUE " |lolcat
+echo  "Thanks For Using My Tools" |lolcat
+sleep 1
+echo  "EKAY ANCURTOTAL On Your System" |lolcat
+
+echo  "See you Again SCRIPTKIDDIES😎 :)..." |lolcat
+echo
+echo  "AIM FOR SOMETHING THAT IS IMPOSSIBLE"
+echo  "THERE IS NO SAFE SYSTEM"
+echo  "HAVE FUN IN CYBERSPACE AND THE REAL WORLD"
+echo  "HACKING IS NOT ALWAYS A CRIME :v"
+figlet ANCURTOTAL | lolcat
+echo "                 #  ANCURTOTAL CYBER TEAM # " | lolcat
+exit
+
+
+;;
+
+*) echo "Sorry, your choice isn't right ... drink coffee first bro :) "
+esac
+done
+done
+
+
+
+
